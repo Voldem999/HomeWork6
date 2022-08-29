12394936c35e0c601241d89b4ef20aa2b0e47af8
Ilya201091 <ZiK201091@ya.ru>
2022-08-29 21:45:12 +0700

    подготовка к ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,13 @@
 import java.util.Random;
 import java.util.stream.IntStream;
-import java.util.Arrays;
 public class Main {
-    int[] arr = generateRandomArray();
-
-    public static int[] generateRandomArray() {
-        java.util.Random random = new java.util.Random();
-        int[] arr = new int[30];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = random.nextInt(100_000) + 100_000;}
-        return arr;
-    }
-       public static void main(String[] args) {
-
+    public static void main(String[] args) {
+        java.util.Random random = new Random();
+        System.out.println("Hello world!");
         int[] month = new int[30];
-        int day1 = month[0];
-        int arr[] = {3, 5, 7, 12};
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum = sum + arr[i];}
-        System.out.println(sum);
-
-
-
-    // generate 100 random number between 0 and 100
-    int[]  randomIntsArray = IntStream.generate(() -> new Random().nextInt(100)).limit(100).toArray();
-    //generate 100 random number between 100 to 200
-    int[]  randomIntsArray2 = IntStream.generate(() -> new Random().nextInt(100) + 100).limit(100).toArray();
+        int i;
+        for (i= 0; i < month.length; i++) {
+            month[i] = random.nextInt(100_000) + 100_000;
+        }
+        System.out.println(month[0] + " сумма в день");
     }}
