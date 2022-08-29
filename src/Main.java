d9dc20fcb99282d82f5178e083e59273a05d62df
Ilya201091 <ZiK201091@ya.ru>
2022-08-29 22:18:50 +0700

    подготовка к ДЗ

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,29 @@ public class Main {
         System.out.println("Hello world!");
         int[] month = new int[30];
         int i;
-        for (i= 0; i < month.length; i++) {
-            month[i] = random.nextInt(100_000) + 100_000;
-        }
+        for (i = 0; i < month.length; i++)
+        {month[i] = random.nextInt(100_000) + 100_000;}
         System.out.println(month[0] + " сумма в день");
+
+        // Задача 1
+
+        int sum = 0;
+        for (i = 0; i < month.length; i++)
+        {sum = sum + month[i];}
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+
+        // Задача 2
+
+        int maxMonth = - 100_000;
+        for (final int current : month) {
+            if (current > maxMonth)
+            {maxMonth = current;}}
+        {System.out.println("Максимальная сумма трат за день составила " + maxMonth + " рублей");}
+
+
+        // Задача 3
+
+
+
+        // Задача 4
     }}
